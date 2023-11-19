896a6840c28c5867849512d07e3be4396baaaec5
shjuka <shjuka@gmail.com>
2023-11-18 20:37:33 -0500

    basic console

---
+++ ConsoleTemplate/Program.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+
+namespace ConsoleTemplate;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+
+        var builder = new ConfigurationBuilder();
+        BuildConfig(builder);
+
+        Log.Logger = new LoggerConfiguration()
+            .ReadFrom.Configuration(builder.Build())
+            .Enrich.FromLogContext()
+            .WriteTo.Console()
+            .CreateLogger();
+
+        Log.Logger.Information("Application Starting");
+
+        var host = Host.CreateDefaultBuilder()
+            .ConfigureServices((context, services) => 
+            { 
+                services.AddTransient<IGreetingService, GreetingService>();
+
+            })
+            .UseSerilog()
+            .Build();
+
+        var svc = ActivatorUtilities.CreateInstance<GreetingService>(host.Services);
+        svc.Run(); 
+    }
+
+    static void BuildConfig(IConfigurationBuilder builder)
+    {
+        builder.SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json",optional:false,reloadOnChange:true)
+            .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETRCORE_ENVIRONMENT")??"Production"}.json", optional: true)
+            .AddEnvironmentVariables();
+
+
+    }
+}
+
